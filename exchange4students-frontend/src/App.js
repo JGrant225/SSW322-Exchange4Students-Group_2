b60daa18164c05dc4482a43edc8d577b869b5412
EdmundYuenStevens <eyuen2@stevens.edu>
2025-03-23 02:18:09 -0400

    Implement React Frontend

---
+++ exchange4students-frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Register from "./Register";
+import Login from "./Login";
+
+function App() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>Exchange4Students</h1>
+      <Register />
+      <hr />
+      <Login />
+    </div>
+  );
+}
+
+export default App;

df1b3791bc04381f8462158935b83d92ec2eadb8
James Grant <jamesgrant2225@gmail.com>
2025-03-25 22:53:18 -0400

    Move main functions to 'Home.jsx' and set up routing in 'App.js'

--- exchange4students-frontend/src/App.js
+++ exchange4students-frontend/src/App.js
@@ -1,58 +1,19 @@
-import React, { useState } from "react";
-import Register from "./Components/Register";
-import Login from "./Components/Login";
-import PostItem from "./Components/PostItem";
-import { jwtDecode } from "jwt-decode";
+import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './Pages/Home'
+import { ItemPosting } from './Pages/ItemPosting'
+import { LoginPage } from './Pages/LoginPage'
 
 function App() {
-  // Track logged-in user and token (in memory only)
-  const [username, setUsername] = useState("");
-  const [token, setToken] = useState("");
-
-  // Logout function clears state
-  const handleLogout = () => {
-    setUsername("");
-    setToken("");
-  };
-
-  // Called when login is successful
-  const handleLoginSuccess = (newToken) => {
-    try {
-      const decoded = jwtDecode(newToken);
-      setUsername(decoded.username);
-      setToken(newToken);
-    } catch {
-      setUsername("");
-      setToken("");
-    }
-  };
-
   return (
-    <div style={{ padding: "2rem" }}>
-      <h1>Exchange4Students</h1>
-
-      {/* If logged in, show logout info */}
-      {username && (
-        <div>
-          <p>Logged in as: <strong>{username}</strong></p>
-          <button onClick={handleLogout}>Logout</button>
-        </div>
-      )}
-
-      {/* Show Register and Login only when not logged in */}
-      {!username && (
-        <>
-          <Register />
-          <hr />
-          <Login onLoginSuccess={handleLoginSuccess} />
-          <hr />
-        </>
-      )}
-
-      {/* Always show PostItem */}
-      <PostItem username={username} token={token} />
-    </div>
-  );
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/itemPosting" element={<ItemPosting/>}/>
+        <Route path="/LoginPage" element={<LoginPage/>}/>
+      </Routes>
+    </Router>
+  ) 
 }
 
 export default App;
--- exchange4students-frontend/src/Pages/itemPosting.jsx
+++ exchange4students-frontend/src/Pages/itemPosting.jsx
@@ -1,6 +1,6 @@
 
 
-export function itemPosting(){
+export function ItemPosting(){
     return (
         <>
             <h1>This is the Item Posting page</h1>
